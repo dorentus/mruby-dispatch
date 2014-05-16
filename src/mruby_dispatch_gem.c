370addfc4e6a9723b2b7c928edf99c4892d9e137
Zhang Yi <zhangyi.cn@gmail.com>
2014-05-16 15:20:11 +0800

    IMPLEMENT: Dispatch.once

--- src/mruby_dispatch_gem.c
+++ src/mruby_dispatch_gem.c
@@ -1,12 +1,41 @@
 #include "mruby.h"
 #include <dispatch/dispatch.h>
 
-void mrb_mruby_dispatch_gem_init(mrb_state *mrb)
+static dispatch_once_t predicate;
+
+static mrb_value
+mrb_dispatch_once(mrb_state *mrb, mrb_value self)
 {
-  mrb_define_module(mrb, "Dispatch");
+  mrb_value blk;
+
+  mrb_get_args(mrb, "&", &blk);
+
+  if (mrb_nil_p(blk)) {
+    mrb_raise(mrb, E_ARGUMENT_ERROR, "no block given");
+  }
+
+  dispatch_once(&predicate, ^{
+    mrb_yield(mrb, blk, mrb_nil_value());
+  });
+
+  return mrb_nil_value();
 }
 
-void mrb_mruby_dispatch_gem_final(mrb_state *mrb)
+void
+mrb_mruby_dispatch_gem_init(mrb_state *mrb)
 {
+  struct RClass *dispatch;
 
+  dispatch = mrb_define_module(mrb, "Dispatch");
+
+  mrb_define_const(mrb, dispatch, "TIME_NOW", mrb_fixnum_value(DISPATCH_TIME_NOW));
+  mrb_define_const(mrb, dispatch, "TIME_FOREVER", mrb_fixnum_value(DISPATCH_TIME_FOREVER));
+
+  mrb_define_class_method(mrb, dispatch, "once", mrb_dispatch_once, MRB_ARGS_NONE());
+}
+
+void
+mrb_mruby_dispatch_gem_final(mrb_state *mrb)
+{
+  predicate = NULL;
 }
--- test/dispatch.rb
+++ test/dispatch.rb
@@ -1,3 +1,11 @@
 assert('Dispatch') do
   assert_equal Dispatch.class, Module
 end
+
+assert('Dispatch.once') do
+  a = 1
+  10.times do
+    Dispatch.once { a += 1 }
+  end
+  assert_equal a, 2
+end

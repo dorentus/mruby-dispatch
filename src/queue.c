f11b6f3c56695c3f6214baccd0cebfad778fb308
Zhang Yi <zhangyi.cn@gmail.com>
2014-05-21 01:00:28 +0800

    IMPLEMENT: Dispatch::Queue.current, .main, .concurrent

--- src/queue.c
+++ src/queue.c
@@ -9,6 +9,15 @@ mrb_dispatch_queue_release(mrb_state *mrb, void *data)
 static struct mrb_data_type mrb_dispatch_queue_type = { "$mrb_dispatch_queue", mrb_dispatch_queue_release };
 
 static mrb_value
+mrb_queue_set_queue(mrb_value self, dispatch_queue_t q)
+{
+  DATA_TYPE(self) = &mrb_dispatch_queue_type;
+  DATA_PTR(self) = q;
+
+  return self;
+}
+
+static mrb_value
 mrb_queue_initialize(mrb_state *mrb, mrb_value self)
 {
   const char *label_str = NULL;
@@ -18,10 +27,7 @@ mrb_queue_initialize(mrb_state *mrb, mrb_value self)
 
   q = dispatch_queue_create(label_str, NULL);
 
-  DATA_TYPE(self) = &mrb_dispatch_queue_type;
-  DATA_PTR(self) = q;
-
-  return self;
+  return mrb_queue_set_queue(self, q);
 }
 
 static mrb_value
@@ -36,6 +42,71 @@ mrb_queue_to_s(mrb_state *mrb, mrb_value self)
   return mrb_str_new_cstr(mrb, label_str);
 }
 
+static mrb_value
+mrb_queue_current(mrb_state *mrb, mrb_value self)
+{
+  mrb_value instance;
+  dispatch_queue_t q;
+  struct RClass *dispatch, *queue;
+
+  dispatch = mrb_module_get(mrb, "Dispatch");
+  queue = mrb_class_get_under(mrb, dispatch, "Queue");
+
+  instance = mrb_obj_new(mrb, queue, 0, NULL);
+  q = dispatch_get_current_queue();
+
+  return mrb_queue_set_queue(instance, q);
+}
+
+static mrb_value
+mrb_queue_main(mrb_state *mrb, mrb_value self)
+{
+  mrb_value instance;
+  dispatch_queue_t q;
+  struct RClass *dispatch, *queue;
+
+  dispatch = mrb_module_get(mrb, "Dispatch");
+  queue = mrb_class_get_under(mrb, dispatch, "Queue");
+
+  instance = mrb_obj_new(mrb, queue, 0, NULL);
+  q = dispatch_get_main_queue();
+
+  return mrb_queue_set_queue(instance, q);
+}
+
+static mrb_value
+mrb_queue_concurrent(mrb_state *mrb, mrb_value self)
+{
+  mrb_value instance;
+  mrb_sym priority;
+  long priority_value;
+  dispatch_queue_t q;
+  struct RClass *dispatch, *queue;
+
+  mrb_get_args(mrb, "|n", &priority);
+  if (priority == mrb_intern_cstr(mrb, "high")) {
+    priority_value = DISPATCH_QUEUE_PRIORITY_HIGH;
+  }
+  else if (priority == mrb_intern_cstr(mrb, "low")) {
+    priority_value = DISPATCH_QUEUE_PRIORITY_LOW;
+  }
+  else if (priority == mrb_intern_cstr(mrb, "background")) {
+    priority_value = DISPATCH_QUEUE_PRIORITY_BACKGROUND;
+  }
+  else {
+    priority_value = DISPATCH_QUEUE_PRIORITY_DEFAULT;
+  }
+
+  dispatch = mrb_module_get(mrb, "Dispatch");
+  queue = mrb_class_get_under(mrb, dispatch, "Queue");
+
+  instance = mrb_obj_new(mrb, queue, 0, NULL);
+
+  q = dispatch_get_global_queue(priority_value, 0);
+
+  return mrb_queue_set_queue(instance, q);
+}
+
 void
 mrb_queue_init(mrb_state *mrb)
 {
@@ -43,6 +114,10 @@ mrb_queue_init(mrb_state *mrb)
 
   dispatch = mrb_module_get(mrb, "Dispatch");
   queue = mrb_define_class_under(mrb, dispatch, "Queue", mrb->object_class);
+
+  mrb_define_class_method(mrb, queue, "current", mrb_queue_current, MRB_ARGS_NONE());
+  mrb_define_class_method(mrb, queue, "main", mrb_queue_main, MRB_ARGS_NONE());
+  mrb_define_class_method(mrb, queue, "concurrent", mrb_queue_concurrent, MRB_ARGS_OPT(1));
 
   mrb_define_method(mrb, queue, "initialize", mrb_queue_initialize, MRB_ARGS_OPT(1));
   mrb_define_method(mrb, queue, "to_s", mrb_queue_to_s, MRB_ARGS_NONE());
--- test/dispatch.rb
+++ test/dispatch.rb
@@ -6,13 +6,36 @@ assert('Dispatch.once') do
   assert_equal a, 2
 end
 
-assert('Dispatch::Queue#to_s with label') do
+assert('Dispatch::Queue#to_s [1]') do
   label = (0...8).map { (65 + rand(26)).chr }.join
   q = Dispatch::Queue.new label
   assert_equal(label, q.to_s)
 end
 
-assert('Dispatch::Queue#to_s without label') do
+assert('Dispatch::Queue#to_s [2]') do
   q = Dispatch::Queue.new
   assert_equal("", q.to_s)
+end
+
+assert('Dispatch::Queue.current') do
+  q = Dispatch::Queue.current
+  assert_equal(Dispatch::Queue, q.class);
+end
+
+assert('Dispatch::Queue.main') do
+  q = Dispatch::Queue.current
+  assert_equal(Dispatch::Queue, q.class);
+end
+
+assert('Dispatch::Queue.concurrent [1]') do
+  %w(high default low background).each do |priority|
+    q = Dispatch::Queue.concurrent priority.to_sym
+    assert_equal(Dispatch::Queue, q.class)
+  end
+end
+
+assert('Dispatch::Queue.concurrent [2]') do
+  q1 = Dispatch::Queue.concurrent :default
+  q2 = Dispatch::Queue.concurrent
+  assert_equal(q1.to_s, q2.to_s)
 end

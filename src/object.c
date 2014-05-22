41071180cd5d2c7e4368c66ca017a72d8a5d838a
Zhang Yi <zhangyi.cn@gmail.com>
2014-05-22 12:52:31 +0800

    IMP: Dispatch::Object#dispatch_object

--- src/object.c
+++ src/object.c
@@ -22,16 +22,29 @@ mrb_dispatch_suspend(mrb_state *mrb, mrb_value self)
   return mrb_nil_value();
 }
 
+static mrb_value
+mrb_get_dispatch_object(mrb_state *mrb, mrb_value self)
+{
+  struct Rclass *os_dispatch_object;
+
+  os_dispatch_object = mrb_class_get(mrb, "OS_dispatch_object");
+
+  return mrb_obj_value(Data_Wrap_Struct(mrb, os_dispatch_object, &mrb_dispatch_object_type, DATA_PTR(self)));
+}
+
 void
 mrb_object_init(mrb_state *mrb)
 {
-  struct RClass *dispatch, *object;
+  struct RClass *dispatch, *object, *os_dispatch_object;
+
+  os_dispatch_object = mrb_define_class(mrb, "OS_dispatch_object", mrb->object_class);
 
   dispatch = mrb_module_get(mrb, "Dispatch");
   object = mrb_define_class_under(mrb, dispatch, "Object", mrb->object_class);
 
   mrb_define_method(mrb, object, "resume!", mrb_dispatch_resume, MRB_ARGS_NONE());
   mrb_define_method(mrb, object, "suspend!", mrb_dispatch_suspend, MRB_ARGS_NONE());
+  mrb_define_method(mrb, object, "dispatch_object", mrb_get_dispatch_object, MRB_ARGS_NONE());
 }
 
 void
